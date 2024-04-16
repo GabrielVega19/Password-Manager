95c30f901b24cbbc6e08fbe6d6a1ddf0c955b7ad
jseabuzz <164675358+jseabuzz@users.noreply.github.com>
2024-04-16 02:26:51 -0400

    Update main.cpp

--- PMServer/main.cpp
+++ PMServer/main.cpp
@@ -7,10 +7,33 @@
 using boost::asio::ip::tcp;
 
 int main(int argc, char* argv[]) {
-    CryptoPP::SHA1 hash;
+    using namespace CryptoPP;
+
+    SHA1 hash;
     std::cout << "Name: " << hash.AlgorithmName() << std::endl;
     std::cout << "Digest size: " << hash.DigestSize() << std::endl;
     std::cout << "Block size: " << hash.BlockSize() << std::endl;
+
+    HexEncoder encoder(new FileSink(std::cout));
+    std::string msg = "Yoda said, Do or do not. There is no try.";
+    std::string digest;
+
+    StringSource(msg, true, new HashFilter(hash, new StringSink(digest)));
+
+    std::cout << "Message: " << msg << std::endl;
+
+    std::cout << "Digest: ";
+    CryptoPP::StringSource(digest, true, new Redirector(encoder));
+    std::cout << std::endl;
+
+    hash.Update((const byte*)msg.data(), msg.size());
+    bool verified = hash.Verify((const byte*)digest.data());
+
+    if (verified == true)
+        std::cout << "Verified hash over message" << std::endl;
+    else
+        std::cout << "Failed to verify hash over message" << std::endl;
+    
     try {
         boost::asio::io_context io_context;
 
